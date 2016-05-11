c95ef121cf95d64835abfcd682820ae3517b720e
igoingdown <fycjmingxing@126.com>
2016-05-11 10:48:02 +0800

    Update user_neighbor.py

--- user_neighbor.py
+++ user_neighbor.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from sklearn.cluster import KMeans
 import numpy as np
 
@@ -61,21 +63,21 @@ class UserNeighbor(object):
                 self.t_event_id_set.add(result[0])
                 self.organizer_set.add(result[4])
 
-                if result[1] in self.category_count_dict:
-                    self.category_count_dict[result[4]] += 1
-                else:
-                    self.category_count_dict[result[4]] = 0
+                if result[1] not in self.category_count_dict:
+                    self.category_count_dict[result[1]] = 0
+                self.category_count_dict[result[1]] += 1
 
-                if result[4] in self.organizer_count_dict:
-                    self.organizer_count_dict[result[4]] += 1
-                else:
+                if result[4] not in self.organizer_count_dict:
                     self.organizer_count_dict[result[4]] = 0
+                self.organizer_count_dict[result[4]] += 1
+
                 latitude_longitude_list.append([float(result[2]), float(result[3])])
         latitude_longitude_array = np.array(latitude_longitude_list)
         cluster_array = KMeans(n_clusters=self.cluster_num,
                                max_iter=500).fit_predict(latitude_longitude_array)
 
         for i in xrange(len(latitude_longitude_list)):
+            print "正在处理第{0}行……".format(i)
             self.event_location_cluster_org_cat_dict[event_id_list[i]] = (latitude_longitude_list[i],
                                                                           cluster_array[i],
                                                                           organizer_list[i],
@@ -95,7 +97,7 @@ class UserNeighbor(object):
                 user_influence_on_categories[user][cat] = 0
 
         for item in self.social_influence_list:
-            user_influence_on_categories[item[0]][self.event_location_cluster_org_cat_dict[item[0]][3]] \
+            user_influence_on_categories[item[0]][self.event_location_cluster_org_cat_dict[item[1]][3]] \
                 += item[2]
 
         for user in list(self.t_user_id_set):
@@ -115,6 +117,8 @@ class UserNeighbor(object):
                 if compare_user == user:
                     pass
                 else:
+                    if compare_user not in user_similarity_dict:
+                        user_similarity_dict[compare_user] = 0
                     for cat in list(self.category_set):
                         user_similarity_dict[compare_user] += user_influence_on_categories[user][cat] * \
                                                               user_influence_on_categories[compare_user][cat]
@@ -123,7 +127,7 @@ class UserNeighbor(object):
             sorted_similarity_list = sorted(user_similarity_dict.iteritems(),
                                             key=lambda d: d[1], reverse=True)
             for i in xrange(self.top_n):
-                self.neighbor_on_cat[user].add(sorted_similarity_list[i][1])
+                self.neighbor_on_cat[user].add(sorted_similarity_list[i][0])
 
     def find_neighbor_on_org(self):
         user_influence_on_organizers = {}
@@ -139,7 +143,7 @@ class UserNeighbor(object):
                 user_influence_on_organizers[user][org] = 0
 
         for item in self.social_influence_list:
-            user_influence_on_organizers[item[0]][self.event_location_cluster_org_cat_dict[item[0]][2]] \
+            user_influence_on_organizers[item[0]][self.event_location_cluster_org_cat_dict[item[1]][2]] \
                 += item[2]
 
         for user in list(self.t_user_id_set):
@@ -159,15 +163,19 @@ class UserNeighbor(object):
                 if compare_user == user:
                     pass
                 else:
+                    if compare_user not in user_similarity_dict:
+                        user_similarity_dict[compare_user] = 0
+
                     for org in list(self.organizer_set):
-                        user_similarity_dict[compare_user] += user_influence_on_organizers[user][org] * \
-                                                              user_influence_on_organizers[compare_user][org]
+                        user_similarity_dict[compare_user] += \
+                            user_influence_on_organizers[user][org] \
+                            * user_influence_on_organizers[compare_user][org]
 
                     user_similarity_dict[compare_user] /= (len(self.organizer_set))**2
             sorted_similarity_list = sorted(user_similarity_dict.iteritems(),
                                             key=lambda d: d[1], reverse=True)
             for i in xrange(self.top_n):
-                self.neighbor_on_org[user].add(sorted_similarity_list[i][1])
+                self.neighbor_on_org[user].add(sorted_similarity_list[i][0])
 
     def find_neighbor_on_reg(self):
         users_influence_on_regions = {}
@@ -228,7 +236,7 @@ class UserNeighbor(object):
                                             key=lambda d: d[1], reverse=True)
 
             for i in xrange(self.top_n):
-                self.neighbor_on_reg[user].add(sorted_similarity_list[i][1])
+                self.neighbor_on_reg[user].add(sorted_similarity_list[i][0])
 
     def generate_social_influence_matrix(self):
         self.generate_t_event_id_v_event_id_dict()
@@ -258,6 +266,15 @@ class UserNeighbor(object):
     to find final neighbor
     '''
     def generate_users_neighbors(self):
+        self.find_regions_categories_organizers()
+        self.generate_social_influence_matrix()
+        print 'finding neighbor on region……'
+        self.find_neighbor_on_reg()
+        print 'finding neighbor on category……'
+        self.find_neighbor_on_cat()
+        print 'finding neighbor on organizer……'
+        self.find_neighbor_on_org()
+        print 'integrating 3 kinds of neighbors……'
         for user in list(self.t_user_id_set):
             self.users_neighbors[user] = (self.neighbor_on_reg[user] & self.neighbor_on_cat[user]) | \
                                          (self.neighbor_on_reg[user] & self.neighbor_on_org[user]) | \
@@ -267,5 +284,4 @@ if __name__ == '__main__':
     cluster_limit_num = 20
     neighbor_top_n = 100
     neighbor_model = UserNeighbor(cluster_limit_num, neighbor_top_n)
-    neighbor_model.find_regions_categories_organizers()
-    neighbor_model.generate_social_influence_matrix()
+    neighbor_model.generate_users_neighbors()
